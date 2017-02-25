4617ff2b04d39045025a1169478684491b4f202b
leondanamenpride <leondanamenpride@yandex.ru>
2017-02-26 01:09:23 +0500

    Initial commit

---
+++ src/main/java/Frontend.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Frontend extends HttpServlet {
+
+    private String login = "";
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+
+    }
+}
